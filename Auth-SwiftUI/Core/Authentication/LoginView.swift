943b13436c420784b06cf691f2ab9eac7c6de30c
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-01-24 21:55:46 +0300

    Create LoginView and import image

---
+++ Auth-SwiftUI/Core/Authentication/LoginView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
