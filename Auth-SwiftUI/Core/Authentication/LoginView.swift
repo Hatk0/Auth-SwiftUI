4d8ea196c65472785a33220c312c1fc4ce0f25cc
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-01-24 22:00:44 +0300

    Create image

--- Auth-SwiftUI/Core/Authentication/LoginView.swift
+++ Auth-SwiftUI/Core/Authentication/LoginView.swift
@@ -4,7 +4,13 @@ struct LoginView: View {
     var body: some View {
         NavigationStack {
             VStack {
+               Image("firebase")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 120)
+                    .padding(.vertical, 32)
                 
+                Spacer()
             }
         }
     }
