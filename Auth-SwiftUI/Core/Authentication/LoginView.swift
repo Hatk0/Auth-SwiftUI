a1f883369b20069e77b7b63e6c0c3c287ca04f65
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-01-25 11:24:29 +0300

    Create InputView and insert it into LoginView

--- Auth-SwiftUI/Core/Authentication/LoginView.swift
+++ Auth-SwiftUI/Core/Authentication/LoginView.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         NavigationStack {
             VStack {
@@ -9,6 +12,20 @@ struct LoginView: View {
                     .scaledToFit()
                     .frame(width: 100, height: 120)
                     .padding(.vertical, 32)
+                
+                VStack(spacing: 25) {
+                    InputView(text: $email,
+                              title: "Email Adress",
+                              placeholder: "example.com")
+                    .textInputAutocapitalization(.none)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                              isSecureField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
                 
                 Spacer()
             }
